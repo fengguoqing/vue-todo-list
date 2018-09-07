cf244d6cec9f1bc2415f06c06b049931de3957dc
fengguoqing <fengguoqing611@gmail.com>
2018-09-07 22:23:14 +0800

    添加资源文件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="test">
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -13,7 +13,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
