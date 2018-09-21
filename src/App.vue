afc16f2cb705172e14648b9778bcf85e357b2807
fengguoqing <fengguoqing611@gmail.com>
2018-09-21 23:28:45 +0800

    整体框架结构完成

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,13 @@ export default {
 </script>
 
 <style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 16px;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
---
+++ src/components/menus.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- 菜单容器 -->
+  <div class="list-todos">
+    <!-- 单个菜单容器 -->
+    <a class="list-todo activeListClass list" v-for="item in items" :key="item.key">
+      <!-- 锁的图标 -->
+      <span class="icon-lock" v-if="item.locked"></span>
+      <!-- 菜单内待完成的项数量 -->
+      <span class="count-list" v-if="item.count > 0">{{item.count}}</span>
+      {{item.title}}
+    </a>
+    <!-- 新增菜单 -->
+    <a class="link-list-new">
+      <span class="icon-plus"></span>
+      新增
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [{
+        title: '星期一',
+        count: 1,
+        locked: true,
+        key: 1,
+      }, {
+        title: '星期二',
+        count: 2,
+        locked: true,
+        key: 2,
+      }, {
+        title: '星期三',
+        count: 3,
+        locked: false,
+        key: 3,
+      }],
+    };
+  },
+};
+</script>
+
+<style lang="less">
+@import "../common/style/menu.less";
+</style>
+
